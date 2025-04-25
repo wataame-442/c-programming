b06d839d850e0201c8d2f731221e9f9acf02a111
wataame-442 <bvn-510-mjd@e-net.nara.jp>
2025-04-25 06:03:55 +0000

    4_25_4.c

---
+++ 4_25_4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("奇数か偶数かを出力します\n数字を入力してください\t");
+    scanf("%d",&num);
+    if (num%2==0){
+        printf("%dは偶数です\n",num);
+    }
+    else{
+        printf("%dは奇数です\n",num);
+    }
+    // 奇数か偶数かを判定しよう
+    return 0;
+}
